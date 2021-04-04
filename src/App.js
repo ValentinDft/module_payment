85347f3a7d0e2ce9a24e5b6f064a70d2043df610
Valentin Duffet <vavadu07@gmail.com>
2021-04-04 18:29:15 +0200

    ajout des img

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Card from "./components/Card"
 import "./App.css"
+import 'antd/dist/antd.css'; 
 
 export default function App() {
   return (
-    <div>
-      
+    <div style={{backgroundColor: "#282c34", height: "100vh"}}>
+      <Card/>
     </div>
   )
 }
